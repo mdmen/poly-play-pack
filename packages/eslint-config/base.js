cc34512c8069aa4be60a70e44d4636a0a8a67f1b
Dmitry Menov <18627661+mdmen@users.noreply.github.com>
2025-01-05 20:33:40 +0600

    Add TypeScript config

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import turboConfig from 'eslint-config-turbo/flat';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -14,6 +15,7 @@ export default [
       },
     },
   },
+  ...turboConfig,
   pluginJs.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
